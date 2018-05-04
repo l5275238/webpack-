2c9fce4a70dc7d71a40b507a4ea11cdb92fd26f1
shiqiwodezuiai <1360789872@qq.com>
2018-05-04 10:11:55 +0800

    gulp代码发布 webpack js目录更改

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './css/index.less'
-
-window.onload = () => {
-    console.log($);
-    let a = document.createElement('a');
-    a.innerHTML = 'test h1';
-    a.href = 'b.html';
-    document.body.appendChild(a);
-};
