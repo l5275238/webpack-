091e81cf976b00bc4de7fe175ca3fbb8542151b5
lhl <1043891613@qq.com>
2018-05-04 10:46:50 +0800

    2018.5.4

---
+++ README.md
@@ -0,0 +1,4 @@
+# WebpackTemplate
+webpack多页面脚手架
+自动计算rem
+引入less
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp')
+var scp2 = require('gulp-scp2');  //发布插件
+var sequence = require('run-sequence');//同步插件11
+
+gulp.task('text', function(){//发布到测试环境
+    return gulp.src('dist/**')
+        .pipe(scp2({
+            host: '192.168.2.4',
+            username: '',
+            password: '',
+            dest: ''
+        }))
+        .on('error', function(err) {
+            throw err;
+        });
+});
+
+
+gulp.task('pro', function(){//发布到生产环境
+    return gulp.src('dist/**')
+        .pipe(scp2({
+            host: '192.168.2.4',
+            username: '',
+            password: '',
+            dest: ''
+        }))
+        .on('error', function(err) {
+            throw err;
+        });
+});
+
+
+gulp.task('default', function(done){
+    sequence(
+        'dev',
+        done
+    )
+});
